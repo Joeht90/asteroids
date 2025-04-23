9abca6f41d85fa9f1f561f1f6f01980e1668545d
joeht90 <joeht90@icloud.com>
2025-04-23 08:56:47 -0700

    finished portions of main.py added circleshape.py

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ from constants import *
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill(color=(0, 0, 0,))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
     
