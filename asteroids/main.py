2e777d0251d38048f0f4e7ba9659fb59a40aaa2b
Joeht90 <joeht90@gmail.com>
2025-11-29 09:30:49 -0800

    fixed jsonl display log bug

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,13 +2,13 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,4 +19,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-# faking a lesson
+
