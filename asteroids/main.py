730c9d1305fbd9dddf153f4c9825b6139f83a661
joeht90 <joeht90@gamil.com>
2025-12-01 19:17:23 -0800

    even angrier noises cause i have not setup my desktop for git lol

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 pygame.init()
 game_clock = pygame.time.Clock()
 dt = 0
@@ -11,11 +12,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.draw(screen)
         screen.fill("black")
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
--- asteroids/player.py
+++ asteroids/player.py
@@ -2,11 +2,9 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 
 
-class Player:
-    def __init__(self, x, y, player_radius=PLAYER_RADIUS):
-        self.x = x
-        self.y = y
-        self.radius = player_radius
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super()__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
     def triangle(self):
@@ -16,3 +14,16 @@ class Player:
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+
+# in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle())
